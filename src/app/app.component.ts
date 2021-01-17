222eadec27be77b80a0d736acff65306ec6696b2
ghachem eya <ghachemeya12@gmail.com>
2021-01-17 18:06:10 +0100

    First Commir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projet-web';
+  title = 'training-app-D';
+  name = "Walid SAAD";
+  //session_name = "Formation Mobile";
+  nbpart: number=0;
+  firstSession= {
+    id: 1,
+    name: 'Formation Angular',
+    track: 'MEAN Stack',
+    date: new Date('2020-11-19'),
+    duree: 3,
+    local: 'Tunis',
+    participants: 0
+  }
+  nbrParticipantsChange (event)
+  {
+   this.nbpart = event.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SessionItemComponent } from './session-item/session-item.component';
+import { SessionItemListComponent } from './session-item-list/session-item-list.component';
+import { InscriptionDisabledDirective } from './inscription-disabled.directive';
+import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+const appRoutes: Routes = [
+  {
+  path: 'list',
+  component: SessionItemListComponent},
+  {
+  path: 'admin',
+  loadChildren: './admin/admin.module#AdminModule'
+  },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent }
+  ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionItemComponent,
+    SessionItemListComponent,
+    InscriptionDisabledDirective,
+    PagenotfoundComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true }
+    ),
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
